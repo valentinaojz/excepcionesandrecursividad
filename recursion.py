3f360d3b7576ddc191556f7bbcd049477b32e86a
valentinaojz <valentina.ojeda@soymas.cl>
2023-08-07 14:12:25 -0400

    recursion

--- recursion.py
+++ recursion.py
@@ -1,8 +1,36 @@
+#la recursividad es la función llamandose a si misma dentro del cuerpo de la función.
 def countdown (num):
-    if num == 0: #todas la
+    if num == 0: #condición de salida 
         print("ka-booom")
     else:
         print(num)
         countdown(num - 1)    
-
 countdown(5)
+
+#todo lo que se puede resolver con recursión se puede resolver de otras maneras pero no todos los problemas  se puede resolver con recursión. 
+
+#countdown2 
+print("----------------------------------------------------")
+
+def countdown2 (num):
+    for number in range (-num,1):
+        if number == 0:
+            print("ka-boom")
+        else:
+            print(-number)
+countdown2(5)         
+
+print("-----------------------------------------------------")
+#countdown3 
+def countdown3 (num):
+    for numbers in range(-num,0):
+        print(-numbers)
+    print("ka-boom") 
+countdown3(6)  
+   
+print("--------------------------------------------------")
+
+def infinite_recursion(word):
+    print(word)
+    infinite_recursion(word)
+infinite_recursion("palabra")
